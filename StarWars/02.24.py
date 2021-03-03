e2af3480101ad5fc1c5a867395d8a7e3592ec64b
JedlikKB <60391120+JedlikKB@users.noreply.github.com>
2021-03-03 12:36:35 +0100

    Add files via upload
    a meteor mozog de nem random

--- StarWars/02.24.py
+++ StarWars/02.24.py
@@ -3,67 +3,67 @@ import random
 import time
 
 def fel():
-    ypozicio=urhajo.ycor()
-    ypozicio+=10
-    urhajo.sety(ypozicio)
+    ypozizcio= urhajo.ycor()
+    ypozizcio += 10
+    urhajo.sety(ypozizcio)
 
 
 def le():
-    ypozicio=urhajo.ycor()
-    ypozicio-=10
-    urhajo.sety(ypozicio)
+    ypozizcio= urhajo.ycor()
+    ypozizcio -= 10
+    urhajo.sety(ypozizcio)
 
 
 def jobbra():
-    xpozicio=urhajo.xcor()
-    xpozicio+=10
-    urhajo.setx(xpozicio)
+    xpozizcio= urhajo.xcor()
+    xpozizcio += 10
+    urhajo.setx(xpozizcio)
 
 
 def balra():
-    xpozicio=urhajo.xcor()
-    xpozicio-=10
-    urhajo.setx(xpozicio)
-
+    xpozizcio= urhajo.xcor()
+    xpozizcio -= 10
+    urhajo.setx(xpozizcio)
 
 space = turtle.Screen()
 space.setup(width=800, height=600)
-space.bgpic("Background.png")
+space.bgpic("background.png")
 space.addshape("sprite.gif")
-space.addshape("meteor1.gif")
-
+space.addshape("meteor2.gif")
 space.tracer(0)
 space.listen()
-space.onkey(fel, "Up")
-space.onkey(le, "Down")
-space.onkey(jobbra, "Right")
-space.onkey(balra, "Left")
+space.onkeypress(balra,"Left")
+space.onkeypress(jobbra,"Right")
+space.onkeypress(fel,"Up")
+space.onkeypress(le,"Down")
+
 
 urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
 urhajo.penup()
 
-meteor1 = turtle.Turtle()
-meteor1.shape("meteor1.gif")
-meteor1.penup()
+meteor_jobb = turtle.Turtle()
+meteor_jobb.shape("meteor2.gif")
+meteor_jobb.penup()
+
+meteor_jobb.setx(400)
+while meteor_jobb.xcor()>-400:
+    space.update()
+    time.sleep(0.1)
+    meteor_mozgas = meteor_jobb.xcor()
+    meteor_mozgas -= 10
+    meteor_jobb.setx(meteor_mozgas)
 
 while True:
+
     space.update()
     time.sleep(0.2)
 
-    if urhajo.ycor() > 300:
+    if urhajo.ycor()>300:
         urhajo.sety(-300)
-
-    if urhajo.xcor() > 400:
+    if urhajo.xcor()>400:
         urhajo.setx(-400)
-
-    if urhajo.ycor() < -300:
-        urhajo.sety(300)
-
-    if urhajo.xcor() < -400:
+    if urhajo.xcor()<-400:
         urhajo.setx(400)
-
-    if urhajo.distance(meteor1.xcor(), meteor1.ycor()) < 15:
-        x = random.randint(-380, 380)
-        y = random.randint(-280, 280) 
-        meteor1.goto(x, y)
+    if urhajo.ycor()<-300:
+        urhajo.sety(300)
